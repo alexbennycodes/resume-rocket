0af343228f5757799d2ed241a7fc5f7b66d7f2f8
Alex <alexbenny2811@gmail.com>
2024-03-19 09:28:26 +0530

    fix: fileuploader not found

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-import path from "path";
-
-const __dirname = new URL(".", import.meta.url).pathname;
-
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse"],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
-    };
-    return config;
   },
 };
 
